709abf7784cfb8b3cebcb3eb11f37476e4cf754f
Deepak Vijayan Pillai <DeepakP@namindia.com>
2019-01-16 09:40:57 +0530

    Updates/fixes

---
+++ src/main/java/com/example/demo/SecurityTokenConfig.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity // Enable security config. This annotation denotes config for spring security.
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private JwtConfig jwtConfig;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable()
+				// make sure we use stateless session; session won't be used to store user's
+				// state.
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				// handle an authorized attempts
+				.exceptionHandling()
+				.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+				// Add a filter to validate the tokens with every request
+				.addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+				// authorization requests config
+				.authorizeRequests()
+				// allow all who are accessing "auth" service
+				.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+				// must be an admin if trying to access admin area (authentication is also
+				// required here)
+				.antMatchers("/wines/**").hasRole("ADMIN")
+				// Any other request must be authenticated
+				.anyRequest().authenticated();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Authorization", "Cache-Control", "Content-Type", "Origin", "Accept"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public JwtConfig jwtConfig() {
+		return new JwtConfig();
+	}
+}
